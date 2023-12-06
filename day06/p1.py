57601094042883ec790bb2d78b97fb5d9e746805
Azan Bin Zahid <47540683+azanbinzahid@users.noreply.github.com>
2023-12-06 11:14:51 +0100

    Add files via upload

---
+++ day06/p1.py
@@ -0,0 +1,19 @@
+import math
+
+with open('in.txt', 'r') as f:
+    data = f.readlines()
+    time = list(map(int, data[0].split(":")[-1].split()))
+    distance = list(map(int, data[1].split(":")[-1].split()))
+
+    def get_final_distance(speed, time):
+        return speed * time
+    
+    records = []
+    for id, t in enumerate(time):
+        count = 0
+        for i in range(t):
+            if get_final_distance(t-i, i) > distance[id]:
+                count+=1
+        records.append(count)
+    
+    print(math.prod(records))
